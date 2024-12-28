2affae3dea0a0d01ab982c6aa9f94ca837955ea7
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-28 09:18:59 +0330

    Implemented the TapeSection component.

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,42 @@
+import StarIcon from "@/assets/icons/star.svg";
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 ">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 ">
+        <div
+          className="flex"
+          style={{
+            maskImage:
+              "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+            WebkitMaskImage:
+              "linear-gradient(to right, transparent, black 10%, black 90%, transparent)",
+          }}
+        >
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold text-sm">
+                  {word}
+                </span>
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
